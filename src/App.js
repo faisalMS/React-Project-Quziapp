75ce1fa0116df20d1b95a785a038e62634b79ae5
FaisalMS <fis445@hotmail.com>
2022-06-26 01:54:43 +0300

    feat: first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Questions from "./pages/Questions";
+import FinalScreen from "./pages/FinalScreen";
+import { Container, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+
+
+function App() {
+  return (
+    <Router>
+      <Container maxWidth="sm">
+        <Box textAlign="center" mt={5}>
+          <Switch>
+            <Route path="/" exact>
+              <Typography variant="h2" fontWeight="bold">
+                Quiz App
+              </Typography>
+              <Home />
+            </Route>
+            <Route path="/questions">
+              <Questions />
+            </Route>
+            <Route path="/score">
+              <FinalScreen />
+            </Route>
+          </Switch>
+        </Box>
+      </Container>
+    </Router>
+  );
+}
+
+export default App;
